dc77e7360e6140cbf9916d34c67534e950d16e69
Omkar8881212 <34083101+Omkar8881212@users.noreply.github.com>
2018-12-23 22:51:23 +0530

    Gui Finalize Login Screen Finalized

--- src/bvjiniolibrarymanagement/FXMLDocumentController.java
+++ src/bvjiniolibrarymanagement/FXMLDocumentController.java
@@ -5,18 +5,28 @@
  */
 package bvjiniolibrarymanagement;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogEvent;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -25,28 +35,72 @@ import javafx.stage.StageStyle;
  * @author Omskamate
  */
 public class FXMLDocumentController implements Initializable {
-    
-    @FXML
+   Stage stage;
+   
+     @FXML
     private Label label;
+
+    @FXML
+    private JFXTextField loginfield;
+
+    @FXML
+    private JFXPasswordField passwordfield;
+
+    @FXML
+    private JFXButton login;
+
+    @FXML
+    private JFXButton login2;
+   
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         try {
-           
-            Stage stage=new Stage();
-            Parent root = FXMLLoader.load(getClass().getResource("Dashboard/LibraryDashBoard.fxml"));
-            
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException ex) {
+            String uname=loginfield.getText();
+            String pass=passwordfield.getText();
+             if(uname.equals("") || pass.equals("")){
+                     Alert alert = new Alert(AlertType.WARNING);
+                     alert.setContentText("Username and Password cannot be blank");
+                     alert.showAndWait();
+                     loginfield.setText("");
+                     passwordfield.setText("");
+             }else{
+            if(uname.equals("admin") && pass.equals("admin")){
+             Stage stage2 = (Stage) login.getScene().getWindow();
+             stage2.close();
+             
+               stage.show();
+            }else{
+                
+               
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.setTitle("Login Failed");
+                alert.setHeaderText("Wrong Username and Password");
+                alert.setContentText("Please Enter Correct Username and Password");
+                Optional<ButtonType> result = alert.showAndWait();
+                 if (result.get() == ButtonType.OK){
+                     loginfield.requestFocus();
+                loginfield.setText("");
+                passwordfield.setText("");
+            }
+                 }
+            }
+        } catch (Exception ex) {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        try {
+              stage=new Stage();
+            AnchorPane root = FXMLLoader.load(getClass().getResource("Dashboard/LibraryDashBoard.fxml"));
+            Scene scene = new Scene(root,1200,600);
+             stage.initStyle(StageStyle.UTILITY);
+            stage.setScene(scene);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ }    
     
 }
